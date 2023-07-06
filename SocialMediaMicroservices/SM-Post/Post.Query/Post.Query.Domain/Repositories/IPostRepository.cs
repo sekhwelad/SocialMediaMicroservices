e3f902da7c17293c32743e0f2043d159ae4a29a4
sekhwelad <25sekhwelad@gmail.com>
2023-07-06 15:43:15 +0200

    Read Database

---
+++ SocialMediaMicroservices/SM-Post/Post.Query/Post.Query.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using Post.Query.Domain.Entities;
+namespace Post.Query.Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task CreateAsync(PostEntity post);
+        Task UpdateAsync(PostEntity post);
+        Task DeleteAsync(Guid postId);
+        Task<PostEntity> GetByIdAsync(Guid postId);
+        Task<List<PostEntity>> ListAllAsync();
+        Task<List<PostEntity>> ListByAuthorAsync();
+        Task<List<PostEntity>> ListWithLikesAsync();
+        Task<List<PostEntity>> ListWithCommentsAsync();
+    }
+}
---
+++ SocialMediaMicroservices/SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContextFactory.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Post.Query.Infrastructure.DataAccess
+{
+    public class DatabaseContextFactory
+    {
+        //private readonly Action<DbContextOptionsBuilder>
+    }
+}
